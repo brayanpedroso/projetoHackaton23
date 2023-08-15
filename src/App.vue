97c1b1f38774468644d68733c1979be67d0d3366
GugsTI <gustavobuenodaccb@gmail.com>
2023-08-15 11:15:23 -0300

    Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,31 @@
 <script setup>
+import React from "react";
+import "./nav.css";
+import {Navlink} from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <NavLink to="/home">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/profile">Profile</NavLink>
+            </li>
+            <li>
+              <NavLink to="/about">About</NavLink>
+              </li>
+              </ul>
+              </nav>
+  )
+}
 
 </script>
 
 <template>
-  a 
+
+   
   
 </template>
 
